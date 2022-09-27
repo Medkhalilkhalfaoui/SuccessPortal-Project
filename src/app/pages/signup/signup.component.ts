1c2dcd3db3cf5d857aa554e960bc7420cd54aaf7
med khalil <khalfaouimohamedkhalil@gmail.com>
2022-09-27 23:49:47 +0100

    v1

---
+++ src/app/pages/signup/signup.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { UserService } from 'src/app/services/user.service';
+import Swal from 'sweetalert2'
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class SignupComponent implements OnInit {
+
+  constructor(private userService:UserService, private snack:MatSnackBar) { }
+  public user = {
+    username:'',
+    password:'',
+    firstName:'',
+    lastName:'',
+    email:'',
+    phone:'',
+  };
+
+  ngOnInit(): void {
+  }
+  formSubmit(){
+    console.log(this.user);
+    if(this.user.username==''||this.user.username== null){
+      //alert("user is required !!");
+      this.snack.open('Username is required !!','',{duration: 3000});
+      return;
+
+    }
+    this.userService.adduser(this.user).subscribe(
+      (data:any)=>{
+        //success
+        console.log(data);
+        //alert('SUCCESS');
+        Swal.fire('Success done !!','user id is '+ data.id,'success')
+
+    },
+    (error)=>{
+      //error
+      console.log(error);
+      //alert('something went wrong');
+      this.snack.open('something went wrong !!','',{duration: 300});
+    }
+    )
+  }
+
+}
