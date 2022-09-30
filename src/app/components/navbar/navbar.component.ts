4ed78a0275f68d8ee28b25a81ee09571b1d3efe5
med khalil <khalfaouimohamedkhalil@gmail.com>
2022-09-30 23:48:30 +0100

    loginconfig

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from 'src/app/services/login.service';
 
 @Component({
   selector: 'app-navbar',
@@ -6,10 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+  isLoggedIn = false;
+  user = null;
 
-  constructor() { }
+  constructor(public login:LoginService) { }
 
   ngOnInit(): void {
+    this.isLoggedIn = this.login.isLoggedIn();
+      this.user = this.login.getUser();
+
+    this.login.loginStatusSubject.asObservable().subscribe((data)=>{
+      this.isLoggedIn = this.login.isLoggedIn();
+      this.user = this.login.getUser();
+
+
+    });
+    
+  }
+  public logout(){
+    this.login.logout();
+   
+    window.location.reload();
+
   }
 
 }
